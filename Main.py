eb5ab5ccbdcdb90d1172d86d1a12ce284b08b57d
sumithar1 <100009512+sumithar1@users.noreply.github.com>
2022-07-25 16:08:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,51 +8,51 @@ class Evaluate:
     # Write your code here
 
 
-  def __init__(self, size):
-    """Inits Evaluate with top, size_of_stack and stack.
+    def __init__(self, size):
+      """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       top:An integer which points to the top most element in the stack.
       size_of_stack: An integer which represents size of stack.
       stack: A list which maintians the elements of stack.
     """
-    self.top = -1
-    self.size_of_stack = size
-    self.stack = []
+      self.top = -1
+      self.size_of_stack = size
+      self.stack = []
 
 
-  def isEmpty(self):
+    def isEmpty(self):
     """
     Check whether the stack is empty.
     Returns:
       True if it is empty, else returns False.
     """
       # Write your code here
-      return len(self.stack) == 0
+        return len(self.stack) == 0
 
 
-  def pop(self):
+    def pop(self):
     """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
     # Write your code here
-    if not isEmpty(self.stack):
-        return self.stack.pop()
+      if not isEmpty(self.stack):
+          return self.stack.pop()
 
 
-  def push(self, operand):
+    def push(self, operand):
     """
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
     """
     # Write your code here
-    if len(self.stack) <= self.size_of_stack:
-        self.stack.append(operand)
+      if len(self.stack) <= self.size_of_stack:
+          self.stack.append(operand)
 
 
-  def validate_postfix_expression(self, expression):
+    def validate_postfix_expression(self, expression):
     """
     Check whether the expression is a valid postfix expression.
     Arguments:
@@ -61,24 +61,24 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     # Write your code here
-    for character in self.expression:
-        if character not in '1234567890' or character not in '+-*/^:
-            return False
-    return True
+        for character in self.expression:
+            if character not in '1234567890' or character not in '+-*/^:
+                return False
+        return True
 
-  def evaluation(self, number_1, number_2, operator):
-        if self.operator == '+':
-            return number_2 + number_1
-        elif self.operator == '-':
-            return number_2 - number_1
-        elif self.operator == '*':
-            return number_2 * number_1
-        elif self.operator == '/':
-            return number_2 / number_1
-        elif self.operator == '^':
-            return number_2 ** number_1
+    def evaluation(self, number_1, number_2, operator):
+          if self.operator == '+':
+              return number_2 + number_1
+          elif self.operator == '-':
+              return number_2 - number_1
+          elif self.operator == '*':
+              return number_2 * number_1
+          elif self.operator == '/':
+              return number_2 / number_1
+          elif self.operator == '^':
+              return number_2 ** number_1
 
-  def evaluate_postfix_expression(self, expression):
+    def evaluate_postfix_expression(self, expression):
     """
     Evaluate the postfix expression
     Arguments:
@@ -87,14 +87,14 @@ class Evaluate:
       The result of evaluated postfix expression.
     """
     # Write your code here
-    for characters in self.expression:
-        if characters in '1234567890':
-            evaluate.push(characters)
-        else:
-            self.number_1 = int(evaluate.pop)
-            self.number_2 = int(evaluate.pop)
-            self.value = evaluate.evaluation(number_1, number_2, characters)
-    print(self.stack[0])
+      for characters in self.expression:
+          if characters in '1234567890':
+              evaluate.push(characters)
+          else:
+              self.number_1 = int(evaluate.pop)
+              self.number_2 = int(evaluate.pop)
+              self.value = evaluate.evaluation(number_1, number_2, characters)
+      print(self.stack[0])
 
 
 # Do not change the following code
