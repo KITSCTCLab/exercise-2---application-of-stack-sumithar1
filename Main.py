aa36264cd435aa9a6ccc4eafc406e07b772b423d
sumithar1 <100009512+sumithar1@users.noreply.github.com>
2022-07-26 22:01:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ class Evaluate:
  #   stack: A List which acts as a Stack.
 
 
-  def _init_(self, size):
+  def __init__(self, size):
   #Inits Evaluate with top, size_of_stack and stack.
   #  Arguments:
   #    size_of_stack: An integer to set the size of stack.
